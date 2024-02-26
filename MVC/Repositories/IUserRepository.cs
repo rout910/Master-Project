242390ea169c70c8b51e02eec1ad4c347bbb6732
Nimbarkshree <shreenimbark8@gmail.com>
2024-02-26 15:18:02 +0530

    user repo,program.c,connstring,npgsql package

--- MVC/Repositories/IUserRepository.cs
+++ MVC/Repositories/IUserRepository.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MVC.Models;
 
 namespace MVC.Repositories
 {
     public interface IUserRepository
     {
-        
+        public void Register(tbluser user);
+        public bool IsUser(string email);
+        public bool Login(tbluser user); 
     }
 }
