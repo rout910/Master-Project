ce09ef50746041cff957b3a5e85b6656f66f9d0c
shrestharani <kondabathinishrestharani@gmail.com>
2024-02-28 10:27:27 +0530

    KendoGrid Insert Crud

--- MVC/Controllers/KendoGridCrudController.cs
+++ MVC/Controllers/KendoGridCrudController.cs
@@ -3,84 +3,77 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MVC.Models;
 using MVC.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MVC.Controllers
 {
-    // [Route("[controller]")]
+   // [Route("[controller]")]
     public class KendoGridCrudController : Controller
     {
-        private readonly ILogger<KendoGridCrudController> _logger;
-        private readonly IEmpRepository _empRepo;
         private readonly IWebHostEnvironment _environment;
-        public KendoGridCrudController(ILogger<KendoGridCrudController> logger,IEmpRepository empRepo,IWebHostEnvironment webHostEnvironment)
+        private readonly IEmpRepository _repo;
+        public KendoGridCrudController( IEmpRepository repo,IWebHostEnvironment webHostEnvironment)
         {
-            _logger = logger;
-              _empRepo = empRepo;
-              _environment = webHostEnvironment;
+            _repo=repo;
+            _environment = webHostEnvironment;
         }
 
-       [Produces("application/json")]
-       [HttpGet]
         public IActionResult Index()
         {
-           
             return View();
         }
-
         [Produces("application/json")]
-       [HttpGet]
+        [HttpGet]
         public IActionResult GetAll()
         {
-            var emp =_empRepo.GetAll();
-            return Json(emp);
+            var cities = _repo.GetAll();
+            return Json(cities);
+        }
+        [HttpPost]
+        public IActionResult add(tblemp emp)
+        {
+            _repo.Insert(emp);
+             return Json(new { success = true});  
         }
         public IActionResult FetchStates()
         {
-            List<tbldept> states = _empRepo.GetDept();
+            List<tbldept> states = _repo.GetDept();
             return Json(states);
         }
-        [HttpPost]
-        public IActionResult add(tblemp stud)
+       [HttpPost]
+public IActionResult UploadImage(IFormFile file)
+{
+    if (file != null && file.Length > 0)
+    {
+        var uploads = Path.Combine(_environment.WebRootPath, "images"); // Assuming you have a folder named 'image' in wwwroot
+        var filePath = Path.Combine(uploads, file.FileName);
+
+        using (var fileStream = new FileStream(filePath, FileMode.Create))
         {
-            _empRepo.Insert(stud);
-             return Json(new { success = true, newCityId = stud.c_empid});  
-        }
-      
-        [HttpPost]
-        public IActionResult UploadPhoto(IFormFile photo)
-        {
-            try
-            {
-                if (photo != null)
-                {
-                    string filename = Guid.NewGuid().ToString() + Path.GetExtension(photo.FileName);
-                    string filepath = Path.Combine(_environment.WebRootPath, "images", filename);
-
-                    using (var stream = new FileStream(filepath, FileMode.Create))
-                    {
-                        photo.CopyTo(stream);
-                    }
-
-                    var stateModel = new tblemp { c_empimage = filename };
-                    _empRepo.Insert(stateModel);
-
-                    return Json(new { success = true, filename });
-                }
-                else
-                {
-                    return Json(new { success = false, message = "No file uploaded" });
-                }
-            }
-            catch (Exception ex)
-            {
-                return Json(new { success = false, message = ex.Message });
-            }
+            file.CopyTo(fileStream);
         }
 
+        var imageUrl = "/images/" + file.FileName; // Assuming your image URL is relative
+        return Json(new { imageUrl });
+    }
+    return Json(new { error = "No file uploaded or file is empty." });
+}
+        [HttpPost]
+        public IActionResult UpdateCity(tblemp city)
+        {
+                _repo.Update(city);
+                return Json(new { success = true });
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+                Console.WriteLine("Received id:" +id);
+                _repo.Delete(id);
+                return Json(new { success = true, message = "City deleted." });
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
